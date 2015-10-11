7abc7227c1c000c824f712971b24e75d0ebb15dd
JovannyEspinal <espinaljovanny@icloud.com>
2015-10-11 14:47:25 -0400

    Storyboard v.05 with CVC

---
+++ FoodbLog/FoodbLog/InstagramImagePicker.h
@@ -0,0 +1,15 @@
+//
+//  InstagramImagePicker.h
+//  FoodbLog
+//
+//  Created by Jovanny Espinal on 10/11/15.
+//  Copyright © 2015 Ayuna Vogel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InstagramImagePicker : UICollectionViewController
+
+
+
+@end
---
+++ FoodbLog/FoodbLog/InstagramImagePicker.m
@@ -0,0 +1,13 @@
+//
+//  InstagramImagePicker.m
+//  FoodbLog
+//
+//  Created by Jovanny Espinal on 10/11/15.
+//  Copyright © 2015 Ayuna Vogel. All rights reserved.
+//
+
+#import "InstagramImagePicker.h"
+
+@implementation InstagramImagePicker
+
+@end
