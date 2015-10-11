d74410a48bbd7d789115bc4aa1bce942eaf47229
JovannyEspinal <espinaljovanny@icloud.com>
2015-10-11 16:03:42 -0400

    Storyboard v.06 CVC

--- FoodbLog/FoodbLog/InstagramImagePicker.m
+++ FoodbLog/FoodbLog/InstagramImagePicker.m
@@ -10,6 +10,34 @@
 
 @implementation InstagramImagePicker
 
+-(void)viewDidLoad{
+    CGFloat leftAndRightPaddings = 32.0;
+    CGFloat numberOfItemsPerRow = 3.0;
+    CGFloat heightAdjustment = 30.0;
+    
+    CGFloat width = (CGRectGetWidth(self.collectionView.frame) - leftAndRightPaddings)/numberOfItemsPerRow;
+    
+    self.collectionViewLayout.itemSize = CGSizeMake(width, width +heightAdjustment)
+}
+
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
+    
+    return 1;
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    
+    return [self.imageURLArray count];
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"TestCell" forIndexPath:indexPath];
+    
+    
+    
+    return cell;
+}
 
 
 @end
