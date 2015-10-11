b9f295bc3dd7ac7c35f8ab961f4632e4fc03198c
ayunav <ayunanyc@gmail.com>
2015-10-11 12:42:22 -0400

    created CreateLogViewController file

--- FoodbLog/FoodbLog/CreateLogViewController.h
+++ FoodbLog/FoodbLog/CreateLogViewController.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  CreateLogViewController.h
 //  FoodbLog
 //
 //  Created by Ayuna Vogel on 10/11/15.
 //  Copyright © 2015 Ayuna Vogel. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "ViewController.h"
 
-@interface ViewController : UIViewController
-
+@interface CreateLogViewController : ViewController
 
 @end
-
--- FoodbLog/FoodbLog/CreateLogViewController.m
+++ FoodbLog/FoodbLog/CreateLogViewController.m
@@ -1,5 +1,5 @@
 //
-//  ViewController.m
+//  CreateLogViewController.m
 //  FoodbLog
 //
 //  Created by Ayuna Vogel on 10/11/15.
@@ -7,13 +7,13 @@
 //
 #import <Parse/Parse.h>
 
-#import "ViewController.h"
+#import "CreateLogViewController.h"
 
-@interface ViewController ()
+@interface CreateLogViewController ()
 
 @end
 
-@implementation ViewController
+@implementation CreateLogViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -30,5 +30,15 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
